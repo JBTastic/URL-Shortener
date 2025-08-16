d8cc7094feebfdd8e9db495cf015d3731106c25e
Julian <167174811+JBTastic@users.noreply.github.com>
2025-08-17 00:08:19 +0200

    feat: add redirecting function on netlify

---
+++ jbtastic-url-shortener/netlify/functions/redirect.js
@@ -0,0 +1,42 @@
+import fetch from "node-fetch";
+
+export async function handler(event, context) {
+  const slug = event.queryStringParameters.slug;
+
+  if (!slug) {
+    return {
+      statusCode: 400,
+      body: "Missing slug"
+    };
+  }
+
+  // Supabase-URL and Anon Key from Environment Variables
+  const SUPABASE_URL = process.env.VITE_SUPABASE_URL;
+  const SUPABASE_KEY = process.env.VITE_SUPABASE_ANON_KEY;
+
+  const response = await fetch(`${SUPABASE_URL}/rest/v1/short_urls?slug=eq.${slug}`, {
+    headers: {
+      "apikey": SUPABASE_KEY,
+      "Authorization": `Bearer ${SUPABASE_KEY}`,
+      "Content-Type": "application/json"
+    }
+  });
+
+  const data = await response.json();
+
+  if (!data || data.length === 0) {
+    return {
+      statusCode: 404,
+      body: "Shortlink not found"
+    };
+  }
+
+  const originalUrl = data[0].original_url;
+
+  return {
+    statusCode: 302,
+    headers: {
+      Location: originalUrl
+    }
+  };
+}
